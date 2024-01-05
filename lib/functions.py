f4234ca9797b6384f76144738e868c3dd1b4cfe4
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-05 18:35:03 -0500

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+print(greet_programmer())
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+print(greet("Naureen"))
+
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+
+print(greet_with_default())
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+sum = add(1,2)
+print(sum)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    else:
+        return number / 2
+
+result = halve(10.5)
+print(result)
